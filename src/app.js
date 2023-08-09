5bed9ac529474a3fcc2a4009458e9a6278464252
Ghazal-Z <137606685+Ghazal-Z@users.noreply.github.com>
2023-08-09 18:51:37 +0330

    Finalized date and time and other descriptions

--- src/app.js
+++ src/app.js
@@ -1,45 +1,28 @@
-let now = new Date();
-
-function formatHour() {
-  let hours = now.getHours();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  return hours;
-}
-function formatMinute() {
-  let minutes = now.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return minutes;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} | ${hours}:${minutes}`;
 }
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let crDay = document.querySelector("#current-date-time");
-
-crDay.innerHTML = `${day} | ${formatHour()}:${formatMinute()}`;
 //
 
-let crCity = document.querySelector("#cur-city");
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-weather");
-  if (searchInput.value) {
-    crCity.innerHTML = `${searchInput.value}`;
-  } else {
-    crCity.innerHTML = `Please enter a city!`;
-  }
-}
 //
 
 function crTemp(response) {
@@ -50,6 +33,7 @@ function crTemp(response) {
   let curWind = document.querySelector("#wind");
   let roundTemp = Math.round(response.data.main.temp);
   let curIcon = document.querySelector("#icon");
+  let dateElement = document.querySelector("#cur-date");
 
   celsiusTemp = response.data.main.temp;
 
@@ -58,6 +42,7 @@ function crTemp(response) {
   curDescription.innerHTML = response.data.weather[0].description;
   curHumidity.innerHTML = response.data.main.humidity;
   curWind.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   curIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
